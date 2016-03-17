515d0b54c54c115dc641c01b52d368cd0b9685c0
Mathias Berwig <mathiasberwig@gmail.com>
2016-03-17 13:52:57 -0300

    Floating Action Button added to SelectImageActivity
    - MaterialSheetFab library dependency added to build.gradle;
    - New drawables;
    - New styles and dimensions based on MaterialSheetFab;

--- app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/activity/SelectImageActivity.java
+++ app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/activity/SelectImageActivity.java
@@ -1,33 +1,32 @@
 package io.github.mathiasberwig.cloudvision.presentation.activity;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.gordonwong.materialsheetfab.MaterialSheetFab;
+import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
+
 import io.github.mathiasberwig.cloudvision.R;
+import io.github.mathiasberwig.cloudvision.presentation.custom_view.FAB;
 
 public class SelectImageActivity extends AppCompatActivity {
+
+    private MaterialSheetFab materialSheetFab;
+    private int statusBarColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_image);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setupToolbar();
+        setupFAB();
     }
 
     @Override
@@ -35,6 +34,15 @@ public class SelectImageActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_select_image, menu);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (materialSheetFab.isSheetVisible()) {
+            materialSheetFab.hideSheet();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -50,5 +58,52 @@ public class SelectImageActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupFAB() {
+        FAB fab = (FAB) findViewById(R.id.fab);
+        View sheetView = findViewById(R.id.fab_sheet);
+        View overlay = findViewById(R.id.dim_overlay);
+        int sheetColor = getResources().getColor(R.color.cardview_light_background);
+        int fabColor = getResources().getColor(R.color.colorAccent);
+        assert fab != null && sheetView != null && overlay != null;
+
+        // Initialize material sheet FAB
+        materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay, sheetColor, fabColor);
+
+        // Set material sheet event listener
+        materialSheetFab.setEventListener(new MaterialSheetFabEventListener() {
+            @Override
+            public void onShowSheet() {
+                // Save current status bar color
+                statusBarColor = getStatusBarColor();
+                // Set darker status bar color to match the dim overlay
+                setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+            }
+
+            @Override
+            public void onHideSheet() {
+                // Restore status bar color
+                setStatusBarColor(statusBarColor);
+            }
+        });
+    }
+
+    private int getStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return getWindow().getStatusBarColor();
+        }
+        return 0;
+    }
+
+    private void setStatusBarColor(int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(color);
+        }
     }
 }
