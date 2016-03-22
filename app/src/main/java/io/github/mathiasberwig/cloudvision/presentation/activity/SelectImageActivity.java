a13c08fbd93fcf45bcca6dda049b5c2260cc43a2
Mathias Berwig <mathiasberwig@gmail.com>
2016-03-22 13:24:19 -0300

    Added Actions to FAB items
    - startCamera and startGallery methods on SelectImageActivity.
    - class PermissionUtils to help us to get the required permissions on Android 6+.
    - Permissions added to Manifest.

--- app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/activity/SelectImageActivity.java
+++ app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/activity/SelectImageActivity.java
@@ -1,24 +1,39 @@
 package io.github.mathiasberwig.cloudvision.presentation.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.gordonwong.materialsheetfab.MaterialSheetFab;
 import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
 
+import java.io.File;
+
 import io.github.mathiasberwig.cloudvision.R;
+import io.github.mathiasberwig.cloudvision.controller.PermissionUtils;
 import io.github.mathiasberwig.cloudvision.presentation.custom_view.FAB;
 
 public class SelectImageActivity extends AppCompatActivity {
+    private static final String TAG = SelectImageActivity.class.getName();
+
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
 
     private MaterialSheetFab materialSheetFab;
     private int statusBarColor;
+
+    public static final String FILE_NAME = "cloud_vision.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +73,27 @@ public class SelectImageActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+            startCamera(null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            // TODO: Send image to GCM
+        }
+        else if(requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            // TODO: Send image to GCM
+        }
     }
 
     private void setupToolbar() {
@@ -105,5 +141,25 @@ public class SelectImageActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(color);
         }
+    }
+
+    public void startGalleryChooser(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a photo"), GALLERY_IMAGE_REQUEST);
+    }
+
+    public void startCamera(View view) {
+        if (PermissionUtils.requestPermission(this, CAMERA_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getCameraFile()));
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        }
+    }
+
+    public File getCameraFile() {
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(dir, FILE_NAME);
     }
 }
