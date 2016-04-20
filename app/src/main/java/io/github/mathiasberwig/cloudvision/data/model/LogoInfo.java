764d5c6f4915b83eeddf492d711b49d4951d3a57
Mathias Berwig <mathiasberwig@gmail.com>
2016-04-20 09:19:31 -0300

    Empty Model LogoInfo added
    Just to set the references. I'll code this class later.

---
+++ app/src/main/java/io/github/mathiasberwig/cloudvision/data/model/LogoInfo.java
@@ -0,0 +1,9 @@
+package io.github.mathiasberwig.cloudvision.data.model;
+
+/**
+ * Class that stores info about a logo.
+ *
+ * Created by mathias.berwig on 20/04/2016.
+ */
+public class LogoInfo {
+}
