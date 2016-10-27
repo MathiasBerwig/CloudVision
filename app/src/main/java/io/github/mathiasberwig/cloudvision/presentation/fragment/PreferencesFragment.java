eb0801a4ea7438b5a63474ffb0661f49de96ecce
Mathias Berwig <mathiasberwig@gmail.com>
2016-10-27 09:49:50 -0200

    Fixed bug where Preferences Fragment have transparent background

--- app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/fragment/PreferencesFragment.java
+++ app/src/main/java/io/github/mathiasberwig/cloudvision/presentation/fragment/PreferencesFragment.java
@@ -2,6 +2,10 @@ package io.github.mathiasberwig.cloudvision.presentation.fragment;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import io.github.mathiasberwig.cloudvision.R;
 
@@ -23,5 +27,23 @@ public class PreferencesFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        // This prevents transparent background
+        if (view != null) {
+            TypedValue background = new TypedValue();
+            getActivity().getTheme().resolveAttribute(android.R.attr.windowBackground, background, true);
+            if (background.type >= TypedValue.TYPE_FIRST_COLOR_INT && background.type <= TypedValue.TYPE_LAST_COLOR_INT) {
+                // windowBackground is a color
+                view.setBackgroundColor(background.data);
+            } else {
+                // windowBackground is not a color, probably a drawable
+                view.setBackground(getActivity().getResources().getDrawable(background.resourceId));
+            }
+        }
+        return view;
     }
 }
